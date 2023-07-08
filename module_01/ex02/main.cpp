c76e1daa9bcec3ba016c490ea092a2688c519930
Daniela Monteiro <danimdf360@gmail.com>
2023-07-08 17:09:16 -0300

    feat: ex02 is ok

---
+++ module_01/ex02/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/08 16:54:44 by dmonteir          #+#    #+#             */
+/*   Updated: 2023/07/08 17:08:45 by dmonteir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+
+int main(void)
+{
+	std::string hi =  "HI THIS IS BRAIN";
+	std::string *stringPTR = &hi;
+	std::string &stringREF = hi;
+
+	std::cout << "Memory Address of var: " << &hi << std::endl;
+	std::cout << "Memory Address of stringPTR: " << stringPTR << std::endl;
+	std::cout << "Memory Address of stringREF: " << &stringREF << std::endl;
+	std::cout << "Value of var: " << hi << std::endl;
+	std::cout << "Value of stringPTR: " << *stringPTR << std::endl;
+	std::cout << "Value of stringREF: " << stringREF << std::endl;
+}
