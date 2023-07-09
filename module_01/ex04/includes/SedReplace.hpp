fb9bbe3bfdfdaff5e512af6fc63141a9799e05b3
Daniela Monteiro <danimdf360@gmail.com>
2023-07-08 21:04:52 -0300

    hotfix: init ex04

---
+++ module_01/ex04/includes/SedReplace.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   SedReplace.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/08 20:30:31 by dmonteir          #+#    #+#             */
+/*   Updated: 2023/07/08 20:48:00 by dmonteir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SED_REPLACE_HPP
+# define SED_REPLACE_HPP
+class SedReplace
+{
+	public:
+	void SedReplace();
+	void ~SedReplace();
+
+	private:
+
+};
+#endif
--- module_01/ex04/srcs/SedReplace.cpp
+++ module_01/ex04/srcs/SedReplace.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   SedReplace.cpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/07 19:04:55 by dmonteir          #+#    #+#             */
-/*   Updated: 2023/07/08 20:54:41 by dmonteir         ###   ########.fr       */
+/*   Created: 2023/07/08 20:30:00 by dmonteir          #+#    #+#             */
+/*   Updated: 2023/07/08 21:00:54 by dmonteir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,13 @@
 #include <fstream>
 #include <stdlib.h>
 
-void checkInputs(std::int numArgs, std::string fileName,
-std::string str1, std::string str2)
+void SedReplace::SedReplace()
 {
-	if(fileName.empty() || str1.empty() || str2.empty())
-	{
-		std::cout << "Not found" << std::endl;
-		std::exit(1);
-	}
+
 }
 
-int main(int argc, char *argv[])
+void SedReplace::~SedReplace()
 {
-	SedReplace sed;
 
-	if (numArgs != 4)
-	{
-		std::cout << "Invalid input! You need to provide a valid file name,"
-		"a string for replacement, and the string you want to replace." << std::endl;
-		std::exit(1);
-	}
-	checkInputs(argv[1], arg[2], argv[3]);
 }
+
