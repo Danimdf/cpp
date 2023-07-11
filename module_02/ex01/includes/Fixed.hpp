fc0cba3dcfd331f907bf41a8acf3727f7451e259
Daniela Monteiro <danimdf360@gmail.com>
2023-07-11 19:53:20 -0300

    feat: list02 ex01 is ok

--- module_02/ex01/includes/Fixed.hpp
+++ module_02/ex01/includes/Fixed.hpp
@@ -6,11 +6,12 @@
 /*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 19:24:25 by dmonteir          #+#    #+#             */
-/*   Updated: 2023/07/11 18:40:13 by dmonteir         ###   ########.fr       */
+/*   Updated: 2023/07/11 19:49:01 by dmonteir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string>
+#include <iostream>
+#include <ostream>
 
 #ifndef FIXED_HPP
 # define FIXED_HPP
@@ -20,12 +21,19 @@ class Fixed
 	Fixed();
 	~Fixed();
 	Fixed(const Fixed &value);
+	Fixed(const int value);
 	Fixed &operator=(const Fixed &value);
+	Fixed(float const value);
 	int getRawBits() const;
 	void setRawBits(int const raw);
+	float toFloat() const;
+	int toInt() const;
+
 
 	private:
 	int _fixed_point;
 	static const int _frac_bits = 8;
 };
+	std::ostream &operator<<(std::ostream& o, Fixed const& fixed);
+
 #endif
--- module_02/ex01/srcs/Fixed.cpp
+++ module_02/ex01/srcs/Fixed.cpp
@@ -6,12 +6,14 @@
 /*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 19:23:08 by dmonteir          #+#    #+#             */
-/*   Updated: 2023/07/11 18:43:35 by dmonteir         ###   ########.fr       */
+/*   Updated: 2023/07/11 19:51:13 by dmonteir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Fixed.hpp"
+#include <cmath>
 #include <iostream>
+#include <ostream>
 
 Fixed::Fixed()
 {
@@ -23,6 +25,18 @@ Fixed::Fixed(const Fixed &value)
 {
 	std::cout << "Copy constructor called" << std::endl;
 	*this = value;
+}
+
+Fixed::Fixed(const int value)
+{
+	std::cout << "Int constructor called" << std::endl;
+	this->_fixed_point = (value << this->_frac_bits);
+}
+
+Fixed::Fixed(float const value)
+{
+	std::cout << "Float constructor called" << std::endl;
+	this->_fixed_point = (int) roundf(value * (1 << this->_frac_bits));
 }
 
 Fixed &Fixed::operator=(const Fixed &value)
@@ -42,11 +56,26 @@ Fixed::~Fixed()
 
 int Fixed::getRawBits(void) const
 {
-	std::cout << "getRawBits member function called" << std::endl;
 	return this->_fixed_point;
 }
 
 void Fixed::setRawBits(int const raw)
 {
 	this->_fixed_point = raw;
+}
+
+float Fixed::toFloat() const
+{
+	return ((float)this->getRawBits() / ((float)(1 << this->_frac_bits)));
+}
+
+int Fixed::toInt() const
+{
+	return (this->_fixed_point >> this->_frac_bits);
+}
+
+std::ostream &operator<<(std::ostream& o, Fixed const& fixed)
+{
+	o << fixed.toFloat();
+	return o;
 }
