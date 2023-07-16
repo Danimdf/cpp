6ff2ff4747e0a5816c06417b700570e9a1bddc80
Daniela Monteiro <danimdf360@gmail.com>
2023-07-16 10:33:09 -0300

    feat: tests for ex02

--- module_02/ex02/srcs/main.cpp
+++ module_02/ex02/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dmonteir <dmonteir@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/07 19:04:55 by dmonteir          #+#    #+#             */
-/*   Updated: 2023/07/12 21:25:44 by dmonteir         ###   ########.fr       */
+/*   Updated: 2023/07/16 10:31:38 by dmonteir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,54 @@ int main(void)
 	std::cout << a++ << std::endl;
 	std::cout << a << std::endl;
 	std::cout << b << std::endl;
-	std::cout << Fixed::max( a, b ) << std::endl;
-	std::cout << Fixed::min( a, b ) << std::endl;
+	std::cout << Fixed::max(a, b) << std::endl;
 
-	//more tests
+	std::cout << "My tests:" << std::endl;
+	Fixed c(77);
+	Fixed d(5.32f);
+	a = c;
+
+	std::cout << "Arithmetic operators" << std::endl;
+	Fixed e = c + a;
+	std::cout << "a = " << a << " b = " << b << " c = " << c << " d = " << d << " e = " << e << std::endl;
+	std::cout << e << std::endl;
+	e = b - d;
+	std::cout << e << std::endl;
+	e = c * a;
+	std::cout << e << std::endl;
+	e = c / d;
+	std::cout << e << std::endl;
+
+	Fixed f = 77;
+	std::cout << "Increment and decrement operators" << std::endl;
+	std::cout << "++c: " << ++c << std::endl;
+	std::cout << "c--: " << c-- << std::endl;
+	std::cout << "d++: " << d++ << std::endl;
+	std::cout << "--e: " << --e << std::endl;
+
+	std::cout << "Comparison operators" << std::endl;
+	std::cout << "a = " << a << " b = " << b << " c = " << c << " d = " << d << " e = " << e <<  " f = " << f << std::endl;
+	std::cout << "b > c: " << (b > c) << std::endl;
+	std::cout << "c < d: " << (c < d) << std::endl;
+	std::cout << "e >= a: " << (e >= a) << std::endl;
+	std::cout << "d <= b: " << (d <= b) << std::endl;
+	std::cout << "a == c: " << (a == c) << std::endl;
+	std::cout << "c == f: " << (c == f) << std::endl;
+	std::cout << "b != c: " << (b != c) << std::endl;
+
+	std::cout << "Int and float conversion" << std::endl;
+	float floatValue = a.toFloat();
+	int intValue = d.toInt();
+	std::cout << "a to float: " << floatValue << std::endl;
+	std::cout << "d to int: " << intValue << std::endl;
+
+	std::cout << "Min and Max" << std::endl;
+	Fixed x(546);
+	Fixed y(66);
+	Fixed min = Fixed::min(x, y);
+	std::cout << "min(x, y): " << min << std::endl;
+	Fixed max = Fixed::max(x, y);
+	std::cout << "max(x, y): " << max << std::endl;
 
 	return 0;
 }
